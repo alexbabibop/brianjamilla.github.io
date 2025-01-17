1f7505e881bbf0d4dff668581e0c0cd5e3c843e5
Brian Jamilla <jamiestun@gmail.com>
2025-01-17 13:47:02 +0800

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -115,3 +115,4 @@ sr.reveal('.home__img, .about__subtitle, .about__text, .skills__img',{delay: 400
 sr.reveal('.home__social-icon',{ interval: 200}); 
 sr.reveal('.skills__data, .work__img, .contact__input',{interval: 200}); 
 >>>>>>> 69c7904 (Initial commit)
+
